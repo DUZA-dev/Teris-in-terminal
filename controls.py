dc27a889375d926e2bfff0953b8deffb11834a66
s_s <self.script.super@gmail.com>
2020-05-26 22:42:01 +0300

    Add new controls key

--- controls.py
+++ controls.py
@@ -15,3 +15,18 @@ def update(figure):
     keyboard.add_hotkey("down", figure.declineCheckConditions)
     keyboard.add_hotkey("left", figure.moving, args=('left',))
     keyboard.add_hotkey("right", figure.moving, args=('right',))
+
+    keyboard.add_hotkey("w", figure.coup)
+    keyboard.add_hotkey("s", figure.declineCheckConditions)
+    keyboard.add_hotkey("a", figure.moving, args=('left',))
+    keyboard.add_hotkey("d", figure.moving, args=('right',))
+
+    keyboard.add_hotkey("k", figure.coup)
+    keyboard.add_hotkey("j", figure.declineCheckConditions)
+    keyboard.add_hotkey("h", figure.moving, args=('left',))
+    keyboard.add_hotkey("l", figure.moving, args=('right',))
+
+    keyboard.add_hotkey("num 8", figure.coup)
+    keyboard.add_hotkey("num 2", figure.declineCheckConditions)
+    keyboard.add_hotkey("num 4", figure.moving, args=('left',))
+    keyboard.add_hotkey("num 6", figure.moving, args=('right',))
